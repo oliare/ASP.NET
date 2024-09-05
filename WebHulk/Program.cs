7f231e7d0e2a2473077f77e7412fd6a6d6ce354e
Olia <oliaopanasiukk@gmail.com>
2024-09-05 22:42:22 +0300

    add and enhance views

--- WebHulk/Program.cs
+++ WebHulk/Program.cs
@@ -76,12 +76,7 @@ app.UseEndpoints(endpoints =>
 
     endpoints.MapControllerRoute(
         name: "default",
-        pattern: "{controller=Main}/{action=Index}/{id?}");
-
-    endpoints.MapControllerRoute(
-      name: "access_denied",
-      pattern: "{controller=Account}/{action=AccessDenied}/{id?}");
-        
+        pattern: "{controller=Main}/{action=Index}/{id?}");        
 });
 #pragma warning restore ASP0014 // Suggest using top level route registrations
 
