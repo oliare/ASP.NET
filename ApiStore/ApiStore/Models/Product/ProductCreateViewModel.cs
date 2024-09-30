d2d9c1f742776b5bf2378ea52ecf0709d7e696c5
Olia <oliaopanasiukk@gmail.com>
2024-09-30 17:02:35 +0300

    bind property for images array

--- ApiStore/ApiStore/Models/Product/ProductCreateViewModel.cs
+++ ApiStore/ApiStore/Models/Product/ProductCreateViewModel.cs
@@ -1,10 +1,13 @@
-﻿namespace ApiStore.Models.Product
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiStore.Models.Product
 {
     public class ProductCreateViewModel
     {
         public string? Name { get; set; }
         public decimal Price { get; set; }
         public int CategoryId { get; set; }
+        [BindProperty(Name = "images[]")]
         public List<IFormFile>? Images { get; set; }
     }
 }
